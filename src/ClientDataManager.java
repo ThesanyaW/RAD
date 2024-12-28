72e841f6ae319aa231d93461163413838de99de8
Thesanya <thesanyawijayasundara490@gmail.com>
2024-12-29 01:57:54 +0530

    Implemented the Client Data Manager Module with Data Encryption.

--- src/ClientDataManager.java
+++ src/ClientDataManager.java
@@ -1,2 +1,40 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.SecureRandom;
+import java.util.Base64;
+
 public class ClientDataManager {
+    private static final String ALGORITHM = "AES";
+
+    public static void main(String[] args) throws Exception {
+        String data = "Sensitive Data";
+        SecretKey secretKey = generateKey();
+
+        String encryptedData = encrypt(data, secretKey);
+        System.out.println("Encrypted Data: " + encryptedData);
+
+        String decryptedData = decrypt(encryptedData, secretKey);
+        System.out.println("Decrypted Data: " + decryptedData);
+    }
+
+    public static SecretKey generateKey() throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
+        keyGen.init(256, new SecureRandom());
+        return keyGen.generateKey();
+    }
+
+    public static String encrypt(String data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decrypt(String encryptedData, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
+        return new String(decryptedBytes);
+    }
 }
